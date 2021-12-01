503cdde18ee1c5fb0b938058f8faddbb5cbe2de5
Emmett Weisz <48953784+emmett-w@users.noreply.github.com>
2021-11-30 22:44:07 -0500

    Copy in parameter selection
    Need to merge with version on Robotics computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.Range;
+
 public class Auto extends Hardware {
+    public int delaySeconds = 0; //seconds to wait for alliance
+    public String startingPosition = "Carousel";
+
     @Override
     public void runOpMode(){
         // Fake values for now. Just showing how to call it
@@ -12,4 +17,41 @@ public class Auto extends Hardware {
 
     }
 
+    public void selectParameters() {
+        String currentParameter = "Delay";
+        while (!gamepad1.a) { //pressing 'a' will end the selection
+            telemetry.addLine("Select " + currentParameter);
+
+            switch (currentParameter) {
+                case "Delay":
+                    if (gamepad1.dpad_up) {
+                        delaySeconds++;
+                    }
+                    if (gamepad1.dpad_down) {
+                        delaySeconds--;
+                    }
+                    delaySeconds = Range.clip(delaySeconds, 0, 30);
+                    telemetry.addLine("delaySeconds = " + delaySeconds);
+                    if (gamepad1.x) { // pressing 'x' sends selector to the next variable
+                        currentParameter = "Starting Position";
+                    }
+                    break;
+                case "Starting Position":
+                    //Choose between "Carousel" and "Warehouse"
+                    if (gamepad1.dpad_up) {
+                        startingPosition = "Carousel";
+                    } else if (gamepad1.dpad_down) {
+                        startingPosition = "Warehouse";
+                    }
+                    telemetry.addLine("startingPosition = " + startingPosition);
+                    if(gamepad1.x) {
+                        currentParameter = "Delay";
+                    }
+                    break;
+            }
+            //Output to telemetry and sleep
+            telemetry.update();
+            sleep(200);
+        }
+    }
 }
