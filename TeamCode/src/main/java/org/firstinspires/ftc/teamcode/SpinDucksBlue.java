31ea01f23bae56b8a46290a297445422ded72fc2
Emmett Weisz <48953784+emmett-w@users.noreply.github.com>
2021-12-06 22:06:42 -0500

    Corrected Spinning ducks
    Now there's one for blue and one for red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinDucksBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpinDucksBlue.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="Spin Ducks Red")
-public class SpinDucksRed extends Hardware {
+@Autonomous(name="Spin Ducks Blue")
+public class SpinDucksBlue extends Hardware {
     @Override
     public void runOpMode() throws InterruptedException {
         hardwareSetup();
         waitForStart();
         //back up slightly
         encoderDrive(0.3,-1,-1,-1,-1);
-        carousel.setPower(-0.7);
+        carousel.setPower(0.7);
         sleep(3000);
         carousel.setPower(0);
         //encoderDrive(0.5,130,130,130,130);
