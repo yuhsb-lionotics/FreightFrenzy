1d3001755efa5a6b475d86af97787eb03b02e6d3
YUHSB Lionotics <lionotics@yuhsbstudents.org>
2021-12-08 09:12:45 -0500

    Added Button class
    to make it easy to detect when a button is newly pressed. This will allow us to use one button as a toggle without wasting time or causing it to go back and forth

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Button.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Button {
+    private boolean currentlyPressed; // Whether the button was pushed during the most recent call of update()
+    private boolean previouslyPressed; // Whether the button was pushed during the previous call of update()
+
+    public Button (boolean firstValue) {
+        this.currentlyPressed = firstValue;
+        this.previouslyPressed = firstValue;
+    }
+
+    //Should be called repeatedly using the while loop in TeleOp
+    public void update(boolean nextValue) {
+        previouslyPressed = currentlyPressed;
+        currentlyPressed = nextValue;
+    }
+
+    public boolean isCurrentlyPressed() {
+        return currentlyPressed;
+    }
+
+    public boolean wasPreviouslyPressed() {
+        return previouslyPressed;
+    }
+
+    public boolean isNewlyPressed () {
+        return (currentlyPressed && !previouslyPressed);
+    }
+
+    public boolean isNewlyReleased() {
+        return (previouslyPressed && !currentlyPressed);
+    }
+}
