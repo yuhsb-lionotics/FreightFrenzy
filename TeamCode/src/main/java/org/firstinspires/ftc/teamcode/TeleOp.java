9fd296a023e2192c04f7b56c163a6c22407442ec
Emmett Weisz <48953784+emmett-w@users.noreply.github.com>
2022-01-09 17:04:12 -0500

    TeleOp Changes
    Added back button to toggle between driving speeds, and put all the controls on gamepad 1 as well as gamepad 2 (in case the usb hub doesn't work, or for convenience of testing with 1 gamepad)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.util.Range;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp")
 public class TeleOp extends Hardware{
+    double maxDrivingPower = 0.8;
+    Button gamepad1back = new Button(false);
+    Button gamepad1a = new Button(false);
     Button gamepad2a = new Button(false);
 
     @Override
@@ -17,15 +20,32 @@ public class TeleOp extends Hardware{
         int clawPos = 1;
         waitForStart();
 
-        //Update the gamepad2a button object with the newest value
+        //Update the button objects with the newest value
+        gamepad1back.update(gamepad1.back);
+        gamepad1a.update(gamepad1.a);
         gamepad2a.update(gamepad2.a);
         while (opModeIsActive()) {
+            gamepad1back.update(gamepad1.back);
+            gamepad1a.update(gamepad1.a);
             gamepad2a.update(gamepad2.a);
-            tankControl(0.8); //CHANGE THIS
+
+            if(gamepad1back.isNewlyPressed()) {
+                if (maxDrivingPower == 0.8) {
+                    maxDrivingPower = 0.4;
+                } else {
+                    maxDrivingPower = 0.8;
+                }
+            }
+
+            telemetry.addData("maxDrivingPower", maxDrivingPower);
+            tankControl(maxDrivingPower);
 
             //turn grabbing on/off when button A is pressed
             if(gamepad2a.isNewlyPressed()) {
                 if(tryingToGrab) stopGrabbing();
+                else startGrabbing(0.5);
+            } else if (gamepad1a.isNewlyPressed()) {
+                if (tryingToGrab) stopGrabbing();
                 else startGrabbing(0.5);
             }
 
@@ -33,7 +53,7 @@ public class TeleOp extends Hardware{
             updateGrabbing();
 
             // Eject the cube if b is pressed
-            if(gamepad2.b && !tryingToGrab){
+            if((gamepad1.b || gamepad2.b) && !tryingToGrab){
                 grabber.setPower(-0.9);
             } else if (!tryingToGrab){
                 grabber.setPower(0);
@@ -41,6 +61,7 @@ public class TeleOp extends Hardware{
             telemetry.addData("TryingToGrab:",tryingToGrab);
             telemetry.addData("Pressed",wheelTouchSensor.isPressed());
             telemetry.addData("gamepad2b",gamepad2.b);
+
             //Control clawPulley
             if (gamepad2.dpad_up) {
                 clawPulley.setPower(0.6);
@@ -52,23 +73,36 @@ public class TeleOp extends Hardware{
                 raiseClawPos(HIGH_POSITION,0.7);
             } else if (gamepad2.dpad_right) {
                raiseClawPos(0, 0.6);
+            } else if (gamepad1.dpad_up) {
+                clawPulley.setPower(0.6);
+                sleep(50);
+            } else if (gamepad1.dpad_down) {
+                clawPulley.setPower(-0.7);
+            } else if (gamepad1.dpad_left) {
+                //raise claw to highest level of shipping hub
+                raiseClawPos(HIGH_POSITION,0.7);
+            } else if (gamepad1.dpad_right) {
+                raiseClawPos(0, 0.6);
             } else if(!clawPulley.isBusy()){
                 clawPulley.setPower(0);
             }
-            //control carousel
-            if (gamepad2.x) {
-                carousel.setPower(1);
-            } else if (gamepad2.y){
-                carousel.setPower(1);
-            }
-            else{
-                carousel.setPower(0);
-            }
-
             //stop the claw pulley if it's at its destination
             if(clawPulley.getMode() == DcMotor.RunMode.RUN_TO_POSITION && !clawPulley.isBusy()) {
                 clawPulley.setPower(0);
                 clawPulley.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
+            //control carousel
+            if (gamepad2.x) {
+                carousel.setPower(1);
+            } else if (gamepad2.y){
+                carousel.setPower(-1);
+            } else if (gamepad1.x) {
+                carousel.setPower(1);
+            } else if (gamepad1.y){
+                carousel.setPower(-1);
+            } else{
+                carousel.setPower(0);
             }
 
             telemetry.addData("ClawPos", clawPos);
