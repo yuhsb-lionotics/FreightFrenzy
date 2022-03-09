69b46c967b11b1426944b8144787307edd9dda59
YUHSB Lionotics <48888106+YUHSB@users.noreply.github.com>
2022-03-08 20:09:56 -0500

    Pid tuning
    and trying to start actual auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/StartingAuto.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+/**
+ * This is a simple teleop routine for testing localization. Drive the robot around like a normal
+ * teleop routine and make sure the robot's estimated pose matches the robot's actual pose (slight
+ * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
+ * exercise is to ascertain whether the localizer has been configured properly (note: the pure
+ * encoder localizer heading may be significantly off if the track width has not been tuned).
+ */
+@Autonomous(name = "roadrunner testing")
+public class StartingAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+        Pose2d starting = new Pose2d(-36,66,Math.toRadians(-90));
+        drive.setPoseEstimate(starting);
+        drive.update();
+        Trajectory forward24 = drive.trajectoryBuilder(starting)
+                .forward(24)
+                .build();
+        Trajectory traj1 = drive.trajectoryBuilder(starting)
+                .splineTo(new Vector2d(-34,24),0)
+                .build();
+        drive.followTrajectory(forward24);
+
+        /*
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("x", poseEstimate.getX());
+        telemetry.addData("y", poseEstimate.getY());
+        telemetry.addData("heading", poseEstimate.getHeading());
+        telemetry.update();
+        sleep(1000);*/
+
+    }
+}
